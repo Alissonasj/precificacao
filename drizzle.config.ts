6e5c20dba592f0924277ca5a01b33beedb2a17c8
alissonasj <alissonsilva.jesus@gmail.com>
2025-03-29 10:44:41 -0300

    adiciona drizzle + configuração inicial

---
+++ drizzle.config.ts
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+import dotenvExpand from 'dotenv-expand';
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+
+dotenvExpand.expand(dotenv.config({ path: '.env.development' }));
+
+export default defineConfig({
+  out: './drizzle',
+  schema: './src/infra/database/schemas/*',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.POSTGRES_URL!,
+    ssl: false
+  }
+});
