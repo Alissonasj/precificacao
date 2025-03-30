464b7290ea446b67ee10f776ecf365dff57d3322
alissonasj <alissonsilva.jesus@gmail.com>
2025-03-29 21:51:57 -0300

    adiciona api `api/v1/status` para retornar status do banco de dados

---
+++ src/app/api/v1/status/route.ts
@@ -0,0 +1,27 @@
+import { database } from '@/infra/database';
+import { sql } from 'drizzle-orm';
+
+export async function GET() {
+  const serverVersion = await database.query('SHOW server_version;');
+  const maxConnections = await database.query('SHOW max_connections;');
+
+  const databaseName = process.env.POSTGRES_DB;
+  const openedConnetions = await database.db
+    .select({
+      value: sql`count('*')`.mapWith(Number)
+    })
+    .from(sql`pg_stat_activity`)
+    .where(sql`datname = ${databaseName}`);
+  const openedConnetionsValue = openedConnetions[0].value;
+
+  const updatedDate = new Date().toLocaleString('pt-BR');
+
+  const serverStatus = {
+    updated_date: updatedDate,
+    server_version: serverVersion.server_version,
+    max_connections: maxConnections.max_connections,
+    opened_connetions: openedConnetionsValue
+  };
+
+  return Response.json(serverStatus);
+}
