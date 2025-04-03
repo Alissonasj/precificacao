8221648bd32bf9739b826fc2fc10843f7b670093
alissonasj <alissonsilva.jesus@gmail.com>
2025-04-03 15:09:51 -0300

    adiciona teste de `API`

--- test/get-materials.ts
+++ test/get-materials.ts
@@ -1,4 +1,15 @@
-import { database } from '@/infra/database/database';
+// import { materialDb } from '@/infra/database/material-db';
 
-const materials = await database.getMaterials();
-console.log(materials);
+import { MaterialSelect } from '@/app/types/material-type';
+
+// const materials = await materialDb.getAll();
+// console.log(materials);
+
+(async () => {
+  const response = await fetch('http://localhost:3000/api/v1/materials');
+  const data: MaterialSelect[] = await response.json();
+
+  data.map((material) => {
+    console.log(material);
+  });
+})();
