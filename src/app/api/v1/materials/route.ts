5ec12fa0f7fa2ee8e84bfee6fd91638c98a3cb2b
alissonasj <alissonsilva.jesus@gmail.com>
2025-04-03 14:59:50 -0300

    adiciona API `api/v1/materials`

---
+++ src/app/api/v1/materials/route.ts
@@ -0,0 +1,44 @@
+import { MaterialInsert, MaterialSelect } from '@/app/types/material-type';
+import { materialDb } from '@/infra/database/material-db';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET() {
+  const materials = await materialDb.getAll();
+
+  return Response.json(materials);
+}
+
+export async function POST(request: NextRequest) {
+  const requestData: MaterialInsert = await request.json();
+  const material = {
+    material: requestData.material,
+    materialGroup: requestData.materialGroup,
+    price: requestData.price
+  };
+
+  materialDb.insert(material);
+
+  return NextResponse.json(material);
+}
+
+export async function PUT(request: NextRequest) {
+  const requestData: MaterialSelect = await request.json();
+  const material = {
+    id: requestData.id,
+    material: requestData.material,
+    materialGroup: requestData.materialGroup,
+    price: requestData.price
+  };
+
+  materialDb.update(material);
+
+  return NextResponse.json(material);
+}
+
+export async function DELETE(request: NextRequest) {
+  const requestData: Pick<MaterialSelect, 'id'> = await request.json();
+
+  materialDb.deleteById(requestData.id);
+
+  return NextResponse.json(requestData.id);
+}
