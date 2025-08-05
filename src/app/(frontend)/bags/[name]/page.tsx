85e1e459f6379d85c56746652fab43752c918b9b
alissonasj <alissonsilva.jesus@gmail.com>
2025-08-05 19:05:38 -0300

    melhora o modal

--- src/app/(frontend)/bags/[name]/page.tsx
+++ src/app/(frontend)/bags/[name]/page.tsx
@@ -1,4 +1,5 @@
 import { getOneBagAction } from '@/actions';
+import PrecificationDialog from '@components/bag/precification-dialog';
 import PrecificationForm from '@components/bag/precification-form';
 import DeleteButton from '@components/bag/ui/delete-button';
 
@@ -32,12 +33,16 @@ export default async function BagDetails({ params }: BagDetailsProps) {
           <li>Criada em: {bag.createdAt.toString()}</li>
           <li>Atualizado em: {bag.updatedAt.toString()}</li>
         </ul>
-        <DeleteButton id={bag.id!} />
+        <div className='flex gap-2.5'>
+          <PrecificationDialog>
+            <PrecificationForm
+              bagName={bag.name}
+              hoursWorked={bag.hoursWorked}
+            />
+          </PrecificationDialog>
+          <DeleteButton id={bag.id} />
+        </div>
       </CardContent>
-      <PrecificationForm
-        bagName={bag.name}
-        hoursWorked={bag.hoursWorked}
-      />
     </Card>
   );
 }
---
+++ src/app/(frontend)/components/bag/precification-dialog.tsx
@@ -0,0 +1,28 @@
+import { Button } from '@ui/shadcn/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from '@ui/shadcn/dialog';
+
+export default function PrecificationDialog({
+  children
+}: {
+  children?: React.ReactNode;
+}) {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant='outline'>Precificar</Button>
+      </DialogTrigger>
+      <DialogContent className='sm:max-w-[425px] max-h-[80vh] overflow-y-auto'>
+        <DialogHeader>
+          <DialogTitle>Adicione um novo Grupo</DialogTitle>
+        </DialogHeader>
+        {children}
+      </DialogContent>
+    </Dialog>
+  );
+}
